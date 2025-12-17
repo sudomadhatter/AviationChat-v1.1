df02f8dc79db0f1471de6c4c6101381a9aef09b3
Sudo Hatter <sudomadhatter@gmail.com>
2025-12-17 21:51:59 +0000

    chore: remove .venv from remote repository

--- .next/static/chunks/src_app_page_tsx_224aa7a7._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_4a5e506b._.js",
-  "static/chunks/src_63828dfd._.js"
-],
-    source: "dynamic"
-});
